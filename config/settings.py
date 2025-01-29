0faf776aa3712234d4b4051f0c5abe25d30b26db
Addhe Warman ( Awan ) <addhe.warman@byorange.co.id>
2025-01-29 16:52:53 +0700

    change endpoint to production

--- config/settings.py
+++ config/settings.py
@@ -2,9 +2,9 @@
 import os
 
 settings = {
-    'API_KEY': os.environ['API_KEY_SPOT_TESTNET_BINANCE'],
-    'API_SECRET': os.environ['API_SECRET_SPOT_TESTNET_BINANCE'],
-    'BASE_URL': 'https://testnet.binance.vision/api',
+    'API_KEY': os.environ['API_KEY_SPOT_BINANCE'],
+    'API_SECRET': os.environ['API_SECRET_SPOT_BINANCE'],
+    'BASE_URL': 'https://api.binance.com/api',
     'TELEGRAM_TOKEN': os.getenv('TELEGRAM_TOKEN'),
     'TELEGRAM_GROUP_ID': os.getenv('TELEGRAM_GROUP_ID'),
     'SYMBOLS': ['BTCUSDT', 'ETHUSDT', 'SOLUSDT'],
@@ -12,7 +12,7 @@ settings = {
     'CACHE_LIFETIME': 60,  # 5 menit
     'MAX_RETRIES': 5,
     'RETRY_BACKOFF': 1,  # 1 detik
-    'BUY_MULTIPLIER': 0.925,
+    'BUY_MULTIPLIER': 0.95,
     'SELL_MULTIPLIER': 1.03,
     'TOLERANCE': 0.01
 }
