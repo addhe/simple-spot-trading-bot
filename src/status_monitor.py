89438e69bd83efda3218f927693e0a7d20dc0d3d
Addhe Warman ( Awan ) <addhe.warman@byorange.co.id>
2025-02-01 07:00:30 +0700

    missing variable ketika refactor

--- src/status_monitor.py
+++ src/status_monitor.py
@@ -1,6 +1,8 @@
 import time
 from src.send_asset_status import send_asset_status
 
+STATUS_INTERVAL = 3600  # 1 jam dalam detik
+
 def status_monitor():
     """Thread terpisah untuk memantau dan mengirim status setiap jam."""
     while True:
