43707e2f6092446dc0385755fb9e93de3faaa013
addhe warman <addhe.warman@gmail.com>
2025-02-24 19:48:20 +0700

    fix telegram send message.py

--- src/send_telegram_message.py
+++ src/send_telegram_message.py
@@ -1,6 +1,9 @@
 # src/send_telegram_message.py
 
 import requests
+import config.settings as settings
+TELEGRAM_TOKEN = settings.TELEGRAM_TOKEN
+TELEGRAM_GROUP_ID = settings.TELEGRAM_GROUP_ID
 
 def send_telegram_message(message):
     url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
